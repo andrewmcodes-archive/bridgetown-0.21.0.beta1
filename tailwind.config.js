fd10c2812b1bc2ed43cda8f9a12ac9055d4feaa6
Andrew Mason <andrewmcodes@protonmail.com>
2021-04-29 23:11:04 -0400

    build: tweak purge config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 
 module.exports = {
-  purge: ["./frontend/**/*.{js,ts}", "./src/**/*.*{erb,rb,html,md,liquid}", "./output/**/*.html"],
+  purge: ["./frontend/**/*.{js,ts}", "./src/**/*.{erb,rb,html,md,liquid}", "./output/**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
